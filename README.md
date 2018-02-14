3ea81ce1b5d0630e75798fa6ae68533f6caaf878
Alex Popescu <the.mindstorm@gmail.com>
2018-02-14 00:26:12 -0800

    add tests for new factory methods for collections (List, Set, Map)

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 Code samples written around Nicolai Parlog's [Code First Java 9 Tutorial - blog@CodeFX](https://blog.codefx.org/java/java-9-tutorial/)
 
+Examples included:
+
+1.  [Private interface methods](/blob/master/src/test/java/com/mypopescu/tut/j9/TestPrivateInterfaceMethods.java)
+2.  [Try with resource and final variables](/blob/master/src/test/java/com/mypopescu/tut/j9/TestTryWithFinalResource.java)
+2.  [New API in `Streams`](/blob/master/src/test/java/com/mypopescu/tut/j9/TestStreamsApi.java)
+3.  [New API in `Optional`](/blob/master/src/test/java/com/mypopescu/tut/j9/TestOptionalApi.java)
+4.  [New factory methods in Collections](/blob/master/src/test/java/com/mypopescu/tut/j9/TestCollectionFactories.java)
+
 ## License
 
 MIT
---
+++ src/test/java/com/mypopescu/tut/j9/TestCollectionFactories.java
@@ -0,0 +1,72 @@
+package com.mypopescu.tut.j9;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static java.util.Map.entry;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Alex Popescu
+ * @since 1.0, 2/13/18 11:35 PM
+ */
+public class TestCollectionFactories {
+    @Test
+    void listOf() {
+        List<String> l = List.of("1", "2", "3");
+
+        assertThrows(UnsupportedOperationException.class,
+                     () -> l.add("4"));
+        assertThrows(UnsupportedOperationException.class,
+                     () -> l.set(0, "4"));
+        assertThrows(UnsupportedOperationException.class,
+                     () -> l.remove(2));
+    }
+
+    @Test
+    void setOf() {
+        IllegalArgumentException iaex = assertThrows(IllegalArgumentException.class,
+                                                     () -> Set.of("1", "1"));
+        assertTrue(iaex.getMessage().contains("duplicate element"));
+
+        Set<String> l = Set.of("1", "2", "3");
+
+        assertThrows(UnsupportedOperationException.class,
+                     () -> l.add("4"));
+        assertThrows(UnsupportedOperationException.class,
+                     () -> l.remove("2"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("createSampleMap")
+    void mapOf(Map<String, String> m) {
+        assertThrows(UnsupportedOperationException.class,
+                     () -> m.put("Alex", "P"));
+        assertThrows(UnsupportedOperationException.class,
+                     () -> m.put("New", "New"));
+        assertThrows(UnsupportedOperationException.class,
+                     () -> m.putIfAbsent("New", "New"));
+        // is this slightly unexpected?
+        // not really as the behavior needs to be
+        assertThrows(UnsupportedOperationException.class,
+                     () -> m.putIfAbsent("Alex", "P"));
+    }
+
+    static Stream<Arguments> createSampleMap() {
+        return Stream.of(
+                Arguments.of(Map.of("Alex", "Alex", "Ana", "Ana")),
+                Arguments.of(Map.ofEntries(entry("Alex", "Alex"),
+                                           entry("Ana", "Ana"))));
+    }
+}
